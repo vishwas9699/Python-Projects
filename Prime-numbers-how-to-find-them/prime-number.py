e35939394563e1a41244631ce735555e2ea41cba
vishwas9699 <vvishwas179@gmail.com>
2020-06-14 19:27:03 +0530

    ic

---
+++ Prime-numbers-how-to-find-them/prime-number.py
@@ -0,0 +1,17 @@
+def isPrime(num):
+    PrimeAnswer = 0
+
+    for e in range(2,num):
+        if (num%e) == 0:
+            PrimeAnswer = False
+            break
+        else:
+            PrimeAnswer = True
+
+    return PrimeAnswer
+
+
+for i in range(1, 20):
+	if isPrime(i + 1):
+			print(i + 1, end=" ")
+print()
