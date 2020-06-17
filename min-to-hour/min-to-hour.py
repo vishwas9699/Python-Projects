1d7c4f9ca139939979919a8916a4bf22d0f47ad1
vishwas9699 <vvishwas179@gmail.com>
2020-06-17 20:30:51 +0530

    ic

--- min-to-hour/min-to-hour.py
+++
@@ -1,6 +0,0 @@
-hour = int(input("Starting time (hours): "))
-mins = int(input("Starting time (minutes): "))
-dura = int(input("Event duration (minutes): "))
-minutes=(mins+dura)%60
-hours=int(hour+(mins+dura)/60)%24
-print(hours," : ",minutes)
