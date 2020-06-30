3bdb63e7affcd5385489ab133296945ce8650616
vishwas9699 <vvishwas179@gmail.com>
2020-06-30 23:12:24 +0530

    ic

---
+++ The-Caesar-Cipher/decrypting a message/decryption.py
@@ -0,0 +1,13 @@
+# Caesar cipher - decrypting a message
+cipher = input('Enter your cryptogram: ')
+text = ''
+for char in cipher:
+    if not char.isalpha():
+        continue
+    char = char.upper()
+    code = ord(char) - 1
+    if code < ord('A'):
+        code = ord('Z')
+    text += chr(code)
+
+print(text)
