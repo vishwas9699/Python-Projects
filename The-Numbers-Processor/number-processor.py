09faf8736d40600b72e8c57aa6ca628930ff6353
vishwas9699 <vvishwas179@gmail.com>
2020-07-01 16:08:30 +0530

    ic

---
+++ The-Numbers-Processor/number-processor.py
@@ -0,0 +1,11 @@
+# Numbers Processor
+
+line = input("Enter a line of numbers - separate them with spaces: ")
+strings = line.split()
+total = 0
+try:
+    for substr in strings:
+        total += float(substr)
+    print("The total is:", total)
+except:
+    print(substr, "is not a number.")
