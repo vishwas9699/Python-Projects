73e1c6cfcbc04ab398a0dc3ccdf7e0328564311d
vishwas9699 <vvishwas179@gmail.com>
2020-07-01 16:09:39 +0530

    ic

--- The-Numbers-Processor/number-processor.py
+++ The-Numbers-Processor/number-processor.py
@@ -1,7 +1,7 @@
 # Numbers Processor
 
 line = input("Enter a line of numbers - separate them with spaces: ") # ask the user to enter a line filled with any number of numbers (the numbers can be floats)
-strings = line.split()
+strings = line.split() # split the line receiving a list of substrings;
 total = 0
 try:
     for substr in strings:
