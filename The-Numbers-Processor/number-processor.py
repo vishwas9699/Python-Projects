40e42bdc2a927ed2512a4f9fe33ba3676334e215
vishwas9699 <vvishwas179@gmail.com>
2020-07-01 16:12:10 +0530

    ic

--- The-Numbers-Processor/number-processor.py
+++ The-Numbers-Processor/number-processor.py
@@ -4,8 +4,8 @@ line = input("Enter a line of numbers - separate them with spaces: ") # ask the 
 strings = line.split() # split the line receiving a list of substrings;
 total = 0 # initiate the total sum to zero;
 try: # as the string-float conversion may raise an exception, it's best to continue with the protection of the try-except block;
-    for substr in strings:
-        total += float(substr)
-    print("The total is:", total)
+    for substr in strings: # iterate through the list...
+        total += float(substr) #  ...and try to convert all its elements into float numbers; if it works, increase the sum;
+    print("The total is:", total) # everything is good so far, so print the sum;
 except:
     print(substr, "is not a number.")
